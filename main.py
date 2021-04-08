34ae011a37de6ed6e0524d8792ef6975b0f40c2a
Tudor Paun <tudorpaun16@gmail.com>
2021-04-08 15:53:51 +0300

    added classes

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('hello')
-print("world")
+
+
+classes=
+{
+    warrior=[60,15,10,15]
+    mage=[30,10,60,20]
+    ranger=[35,35,15,15]
+    bandit=[40,20,10,30]
+}
+#gameloop
+playerstats=[5,5,5,5]
+name=str(input('Care este numele tau?'))
+print(f'')
