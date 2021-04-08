d829cefd192f4ab6fc203694237d0fc90aa53ef0
BuildTools <unconfigured@null.spigotmc.org>
2021-04-08 23:20:31 +0300

    Added stuf

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import random, time
 mobs={
-    'mimic':[45,20,10,5],
-    'dryad':[60,5,60,20],
-    'chompy dingus':[50,10,10,10],
-    'mind flayer':[70,15,30,20],
-    'goblin':[10,5,10,55],
+    'mimic':[60,10,10,40],
+    'dryad':[80,10,60,30],
+    'chompy dingus':[50,10,10,25],
+    'mind flayer':[100,15,30,20],
+    'goblin':[20,5,10,70],
     'dummy':[100,10,10,10]
 }
 classes={
     'warrior':[60,20,10,10],
     'mage':[30,10,60,20],
     'ranger':[35,35,15,15],
-    'bandit':[30,15,5,50]
+    'bandit':[35,15,10,40]
     
 }
 def eattack(hp,ad,selflck,enemylck):
@@ -38,7 +38,7 @@ def eattack(hp,ad,selflck,enemylck):
     #print for debug
     #print(f'HP a ramas {hp} dupa un atac care a dat {finalDmg}')
     return hp
-def pattack(hp,ad,selflck,enemylck):
+def pattack(hp,ad,selflck,enemylck,isw,php):
     hit=random.randint(1,100)
     dodge=random.randint(1,100)
     hit=hit+selflck
@@ -51,10 +51,14 @@ def pattack(hp,ad,selflck,enemylck):
             damageOne=random.randint(int(ad/2),ad)
             damageTwo=random.randint(int(ad/2),ad)
             finalDmg=damageOne+damageTwo
+            if isw==1 and php < 30:
+                finalDmg += finalDmg//4
             hp=hp-finalDmg
             print(f'Crit!\nAi dat {finalDmg} damage!')
         else:
             finalDmg=random.randint(int(ad/2),ad)
+            if isw==1 and php < 30:
+                finalDmg += finalDmg//4
             hp=hp-finalDmg
             print(f'Ai dat {finalDmg} damage!')
     else:
@@ -73,7 +77,6 @@ def deathCheck(playerHP,mobHP):
     return True
 
 
-
 #game
 name=str(input('Care este numele tau?'))
 print(f'{name},alege-ti o clasa intre warrior, mage , ranger si bandit.')
@@ -84,21 +87,25 @@ while playerClassChoice not in classes:
     playerClassChoice=playerClassChoice.lower()
 playerClass=playerClassChoice
 playerStats=classes[playerClass]
+iswarrior = 0
+if playerClass == "warrior":
+    iswarrior = 1
 maxHP=playerStats[0]
-enemyStats=mobs['dummy']
-moves=['attack','heal','shield']
+enemyName, enemyStats = random.choice(list(mobs.items()))
+print(f"Te bati cu un {enemyName}.")
+moves=['attack','heal','special']
 movesString=' '.join([str(elem) for elem in moves])
 #enemyStats = [ int(x) for x in enemyStats ]
 #playerStats = [ int(x) for x in playerStats ]
 while(deathCheck(playerStats[0],enemyStats[0])):
-    print(f'Tu ai {playerStats[0]}HP iar dummy are {enemyStats[0]}HP')
+    print(f'Tu ai {playerStats[0]}HP iar {enemyName} are {enemyStats[0]}HP')
     #playermoves
     moveChoice=str(input('Poti alege din urmatoare lista de miscari:'+' '+movesString.capitalize()+' '))
     moveChoice=moveChoice.lower()
     while(moveChoice not in moves):
         moveChoice=str(input('Miscare invalida.Poti alege din urmatoarea lista de miscari:'+' '+movesString.capitalize()+' '))
     if moveChoice=='attack':
-        enemyStats[0]= pattack(enemyStats[0],playerStats[1],playerStats[3],enemyStats[3])
+        enemyStats[0]= pattack(enemyStats[0],playerStats[1],playerStats[3],enemyStats[3],iswarrior,playerStats[0])
         if enemyStats[0]<=0:
             break
     if moveChoice=='heal':
